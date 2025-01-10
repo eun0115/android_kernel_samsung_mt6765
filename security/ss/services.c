b8532f85c0d5d99a1ba7b59280fd2930fc5a15bd
eun0115 <gianpaoloestacio5@gmail.com>
2025-01-10 21:07:14 +0800

    selinux: Drop CONFIG_ALWAYS_ENFORCE
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- security/ss/services.c
+++ security/ss/services.c
@@ -750,13 +750,6 @@ out:
 	kfree(o);
 	kfree(n);
 	kfree(t);
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP 
-		return 0;
-// ] SEC_SELINUX_PORTING_COMMON
 	return -EPERM;
 }
 
@@ -1655,13 +1648,6 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-// [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP
-		return 0;
-// ] SEC_SELINUX_PORTING_COMMON
 	return -EACCES;
 }
 
@@ -1957,14 +1943,6 @@ static inline int convert_context_handle_invalid_context(
 	struct policydb *policydb = &state->ss->policydb;
 	char *s;
 	u32 len;
-
-// [ SEC_SELINUX_PORTING_COMMON 
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
-	if (!selinux_enforcing) // SEC_SELINUX_PORTING_COMMON Change to use RKP
-		return -EINVAL;
-		// ] SEC_SELINUX_PORTING_COMMON
 
 	if (!context_struct_to_string(policydb, context, &s, &len)) {
 		pr_warn("SELinux:  Context %s would be invalid if enforcing\n",
